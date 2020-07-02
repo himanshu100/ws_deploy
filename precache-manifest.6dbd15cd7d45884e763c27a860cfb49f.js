afa740aa32fd7b4a7a3d2c16b1f4146710b775cc
Himanshu <himanshu.bist@hotmail.com>
2020-07-02 17:12:47 +0530

    Updates

---
+++ precache-manifest.6dbd15cd7d45884e763c27a860cfb49f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865db4b7a831c8f6a5b580893987b063",
+    "url": "/ws_deploy/index.html"
+  },
+  {
+    "revision": "a65861a33f02b063a1bc",
+    "url": "/ws_deploy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9c291a87dc4487823f28",
+    "url": "/ws_deploy/static/css/main.a187d50b.chunk.css"
+  },
+  {
+    "revision": "a65861a33f02b063a1bc",
+    "url": "/ws_deploy/static/js/2.f5e188db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ws_deploy/static/js/2.f5e188db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c291a87dc4487823f28",
+    "url": "/ws_deploy/static/js/main.7fbd10e1.chunk.js"
+  },
+  {
+    "revision": "ba5d18716eb3e5fb0b41",
+    "url": "/ws_deploy/static/js/runtime-main.b189224a.js"
+  }
+]);
